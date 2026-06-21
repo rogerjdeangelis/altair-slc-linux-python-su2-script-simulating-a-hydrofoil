80f7b4e68d7d51b12b8e5c3aeb3422d39f2abd82
Roger DeAngelis <rogerjdeangelis@gmail.com>
2026-06-21 07:09:17 -0700

    Delete su2_env.sh

--- su2_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create SU2 environment file using echo commands (variable not available yet)
-echo 'export SU2_HOME="$HOME/SU2"' > ~/su2_channel_flow/su2_env.sh
-echo 'export SU2_RUN="$HOME/SU2_install/bin"' >> ~/su2_channel_flow/su2_env.sh
-echo 'export PATH="$SU2_RUN:$PATH"' >> ~/su2_channel_flow/su2_env.sh
-echo 'export PYTHONPATH="$SU2_RUN:$PYTHONPATH"' >> ~/su2_channel_flow/su2_env.sh
-chmod +x ~/su2_channel_flow/su2_env.sh
-echo "SU2 environment file created at ~/su2_channel_flow/su2_env.sh"
